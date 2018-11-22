de39403c9d1b26214c30c1b9dbd49065fa9f240c
bhoomc <bhoomc@hotmail.com>
2018-11-23 00:48:26 +0700

    add and commit

--- PrintGrade.c
+++ PrintGrade.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main()
 {
-  char Name[30][40] = {"James Matthews", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"};
+  char Name[30][40] = {"James Matthews", "Yoshida", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"};
   int ID[30] = {5301, 5302, 5303, 5304, 5305, 5306};
   float GPA[30] = {3.73, 2.35, 3.65, 3.24, 1.32, 3.26};
   int N = 6, i;
@@ -11,7 +11,7 @@ void main()
   printf("ID----Name------------------------GPA\n");
   for (i=0;i<N; i++)
     printf("%5d %-25s %5.2f\n", ID[i], Name[i], GPA[i]);
-  printf("-------------------------------------\n");
+  printf("+++++++++++++++++++++++++++++++++++++\n");
   printf("Enter Student ID to Search (0 to End): ");
   scanf("%d", &SearchID);
   while (SearchID)
